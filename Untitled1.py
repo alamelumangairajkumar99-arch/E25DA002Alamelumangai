d13db8df656b4a96fc2cd392c119f0b4ac0f9cb5
alamelumangairajkumar99-arch <alamelumangairajkumar99@gmail.com>
2025-09-23 09:14:30 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+import pandas as pd
+data = {
+    "Name": ["Arun", "Meena", "Raj", "Priya"],
+    "Age": [25, 30, 22, 28],
+    "Department": ["HR", "IT", "Finance", "Marketing"],
+    "Salary": [40000, 50000, 35000, 45000]}
+df = pd.DataFrame(data)
+print(df)
+df.to_csv("C:\\Users\\akila\\OneDrive\\Desktop\\sea.csv", index=False)
+
+print("Data saved to sea")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
